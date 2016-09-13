dc41ff80cdaec2d4c83f594b582fa513314e6dcf
Martin Leopold <m@martinleopold.com>
2016-09-13 12:26:30 +0200

    Frontend: Added global jquery and foundation (without CDN)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,8 +5,14 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
 
+// init foundation
+declare var $;
+$(document).foundation();
+
+// init environment
 if (environment.production) {
   enableProdMode();
 }
 
+// init app
 platformBrowserDynamic().bootstrapModule(AppModule);
