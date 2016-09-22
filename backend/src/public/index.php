d81d6aa34cb54fdd9fdecd6aeb382adfd1031b1c
Martin Leopold <m@martinleopold.com>
2016-09-22 17:57:09 +0200

    Temporary CORS fix

--- backend/src/public/index.php
+++ backend/src/public/index.php
@@ -3,6 +3,11 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+// TEMPORARY CORS FIX
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'); // allow certain headers
+
 //echo 'Current PHP version: ' . phpversion();
 
 //phpinfo();
