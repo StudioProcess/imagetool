3b42ec7db0785ab3f2a0f9679b22a6879cf22a77
Martin Leopold <m@martinleopold.com>
2016-09-30 16:18:46 +0200

    Frontend: Token processing function

--- frontend/src/app/backend.service.ts
+++ frontend/src/app/backend.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class BackendService {
@@ -10,18 +11,35 @@ export class BackendService {
 
   private token: string = null;
 
+  // Process response for token
+  // Works with Observable.map and Observable.catch
   private processToken(response) {
-    console.log('processToken:', response);
-    return response;
+    if ( response.headers.get('Content-Type') == 'application/json') {
+      let responseJSON = response.json();
+      if (responseJSON.token) this.token = responseJSON.token;
+    }
+    if (!response.ok) return Observable.throw(response); // for use with catch
+    return response; // for use with map
   }
 
   test() {
-    this.login('lol', 'lol').subscribe();
+    this.login('lol', 'lol').subscribe({
+      next: (res) => {
+        console.log('next', res);
+      },
+      error: (res) => {
+        console.log('error', res);
+      },
+      complete: () => {
+        console.log('complete');
+      }
+    });
   }
 
-  login(email:string, password:string) {
-    return this.http.post( `${this.API_URL}/login`, {email, password} )
-      .map(this.processToken);
+  login(email:string, password:string) : Observable<any> {
+    return this.http.post( `${this.API_URL}/login`, {email, password})
+      .map(this.processToken)
+      .catch(this.processToken);
   }
 
   logout() {
