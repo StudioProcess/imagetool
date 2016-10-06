6da31bba0a02bc6bc8aacf0337e586adedf26e8a
Martin Leopold <m@martinleopold.com>
2016-10-06 20:14:10 +0200

    Fixed temporary CORS fix

--- backend/src/public/index.php
+++ backend/src/public/index.php
@@ -6,7 +6,7 @@ ini_set('display_errors', 1);
 // TEMPORARY CORS FIX
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'); // allow certain headers
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, X-Access-Token'); // allow certain headers
 
 //echo 'Current PHP version: ' . phpversion();
 
