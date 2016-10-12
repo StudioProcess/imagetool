55f9f7500253d234824cb65094961ccea38fdcfc
Martin Leopold <m@martinleopold.com>
2016-10-12 17:40:53 +0200

    Resume session on app init (server session + last route)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
 import { BackendService } from './backend.service';
 import { SessionService } from './session.service';
 
@@ -10,23 +11,70 @@ import { SessionService } from './session.service';
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
-
+  loading = true;
+  
   constructor(
     private router: Router,
-    private api: BackendService,
+    private backend: BackendService,
     private session: SessionService
   ) {
     console.log('SESSION:', session.get());
-    // api.test();
   }
   
   ngOnInit() {
-    // Always keep stored route up to date
-    this.router.events
-      .filter(e => e instanceof NavigationEnd)
-      .subscribe((e) => {
-        console.log('ROUTE:', e.url);
-        this.session.store({route: e.url});
+    // Resume the session (if possible)
+    this.resumeSession().finally(() => {
+      this.loading = false;
+      
+      // Keep stored route up to date
+      this.router.events
+        .filter(e => e instanceof NavigationEnd)
+        .subscribe((e) => {
+          console.log('ROUTE:', e.url);
+          this.session.store({route: e.url});
+      });
+      
+      console.log("RESUME DONE");
+    }).subscribe();
+  }
+  
+  resumeSession() {
+    /*
+     get user data
+     -> error (401 unauthorized)
+        -> navigate to login page
+     -> success
+        -> get images
+           -> save to local session
+           -> navigate to stored route
+     */
+    
+    // Update local user data
+    let updateUser = this.backend.userData().do(res => {
+      // console.log('update user', res);
+      this.session.store({userData: res.json().data});
+    });
+    
+    // Update local image data
+    let updateImages = this.backend.getImages().do(res => {
+      // console.log('update images', res);
+      this.session.store({images: res.json().data.last_uploaded_images});
+    });
+    
+    return updateUser.switchMapTo(updateImages).do(res => {
+      console.log('resume success', res.json());
+      let url = this.session.get().route;
+      if (!url) url = '/upload';
+      console.info('resuming to', url);
+      this.router.navigateByUrl(url);
+    }).catch(err => {
+      if (err.status == 401) { // Unauthorized i.e. not logged in
+        console.info('No valid token, redirecting to login')
+        this.router.navigate(['login']);
+      } else {
+        console.error('resume error', err);
+      }
+      return Observable.empty();
     });
   }
 }
