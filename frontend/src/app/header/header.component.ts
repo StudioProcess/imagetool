a6e547f9adc248121a33640d91b87240f53543c7
RESL <moritz@resl.xyz>
2016-10-12 09:57:51 +0200

    Frontend: Added individual Username to HeaderComponent
    closes #40
    tackles #36

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../session.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  userName: string;
+
+  constructor(private session: SessionService) { }
 
   ngOnInit() {
+    this.userName = this.session.get().userData.name;
+
   }
 
 }
--- frontend/src/app/session.service.ts
+++ frontend/src/app/session.service.ts
@@ -3,16 +3,16 @@ import { Injectable } from '@angular/core';
 interface SessionData {
   route?: string;
   token?: string;
-  userData?: {};
+  userData?: any;
   images?: any[];
   selectedImage?: any;
 }
 
 @Injectable()
 export class SessionService {
-  
+
   private data: SessionData;
-  
+
   constructor() {
     this.data = {
       route: null,
@@ -23,24 +23,24 @@ export class SessionService {
     };
     this.retrieve();
   }
-  
+
   store(data: SessionData = {}): SessionData {
     Object.assign(this.data, data);
     localStorage.setItem('session', JSON.stringify(this.data));
     return this.data;
   }
-  
+
   // Alias of store
   set(data: SessionData = {}): SessionData {
     return this.store(data);
   }
-  
+
   retrieve(): SessionData {
     let data = JSON.parse( localStorage.getItem('session') );
     if (data) this.set(data);
     return this.data;
   }
-  
+
   get(): SessionData {
     return Object.assign({}, this.data); // Return copy of data
   }
