72a71f5d061137b540270e80a59a65632cce19f1
RESL <moritz@resl.xyz>
2016-10-12 11:37:50 +0200

    Frontend: SessionService added to NavigationComponent for further use (userColor)

--- frontend/src/app/navigation/navigation.component.ts
+++ frontend/src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../session.service';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  userColor: string;
+
+  constructor(private session: SessionService) { }
 
   ngOnInit() {
+    this.userColor = this.session.get().userData.theme_color;
   }
 
 }
