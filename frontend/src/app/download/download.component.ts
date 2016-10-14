8538074e3175da8159af465be6a20d2494d177c3
RESL <moritz@resl.xyz>
2016-10-14 13:11:58 +0200

    Frontend: Added view for empty images array state (Download Component)

--- frontend/src/app/download/download.component.ts
+++ frontend/src/app/download/download.component.ts
@@ -9,11 +9,17 @@ import { BackendService } from '../backend.service';
 })
 export class DownloadComponent implements OnInit {
   images = [];
+  emptyImagesArray: boolean;
 
   constructor(private backend: BackendService, private session: SessionService) { }
 
   ngOnInit() {
     this.images = this.session.get().images;
+    if(this.images.length == 0){
+      this.emptyImagesArray = true;
+    } else {
+      this.emptyImagesArray = false;
+    }
   }
 
 }
