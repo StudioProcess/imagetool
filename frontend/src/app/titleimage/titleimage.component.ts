0034b685b2402a463eeb84fc806637a8ee930f04
RESL <moritz@resl.xyz>
2016-10-14 12:31:37 +0200

    Frontend: Added view for empty images array state

--- frontend/src/app/titleimage/titleimage.component.ts
+++ frontend/src/app/titleimage/titleimage.component.ts
@@ -11,11 +11,17 @@ import { BackendService } from '../backend.service';
 export class TitleimageComponent implements OnInit {
   images = [];
   selectedImage;
+  emptyImagesArray: boolean;
 
   constructor(private backend: BackendService, private session: SessionService) {}
 
   ngOnInit() {
     this.images = this.session.get().images;
+    if(this.images.length == 0){
+      this.emptyImagesArray = true;
+    } else {
+      this.emptyImagesArray = false;
+    }
     this.selectedImage = this.session.get().selectedImage;
   }
 
